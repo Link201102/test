3f6362d969a4c9e5d8ae422b48179f65ac0c3c61
Link201102 <fsasha11@mail.ru>
2025-02-22 10:24:55 +0300

    coonection to database

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
